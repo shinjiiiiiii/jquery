46a973bb5c55fa1d128e1a917c963b181fb87436
Ugo dauv <udauvergne@edenschool.fr>
2023-10-09 11:17:25 +0200

    all

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,51 @@
 import '../css/style.css';
 import '../css/reset.css';
+
+
+// $(".menu").replaceWith('<a href="https://www.edenschool.fr/">lien ecole</a>')
+
+
+
+// $(document).ready(function(){
+//     console.log($("td").text());
+// })
+
+// $( "td" ).each(function() {
+//     console.log($(this).text());
+//   });
+
+// $(document).ready(function (e) {
+//     $('tr:odd').css('background-color','blue')
+    
+//     $('input:submit').on("click", function (e){
+//         console.log($("input:checked").val());
+//         e.preventDefault()
+//     })
+// })
+
+
+
+// $(document).ready(function (e) {
+//     $('h2').css('color','red')
+//     $('.syntax').addClass('fondcouleur')    
+//     $('h2').each(function (e) {
+    
+//         $('.menu').append(`<li><a href ="#${$(this).attr("id")}">${$(this).attr("id")}</a></li>`)
+//     })
+// })
+
+
+
+// $('#inp').click(function (e) {
+//     $('#label').fadeOut('slow')
+// })
+
+// $('#inp2').click(function (e) {
+//     $('#label2').fadeOut('slow')
+// })
+
+// $('input').on('focus',function (e) {
+//     // console.log($(this).attr('id'));
+//     $(`label[for=${$(this).attr('id')}]`).fadeOut(3000)
+    
+// })
